2863122e177c57fc2aff86f5ff4291003fa6834d
Ramdani <pukipukimay289@gmail.com>
2024-08-12 10:47:35 +0700

    import Library firbase

---
+++ main.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTyJmWMigFqEbf12BF9yrOVQh1yWeoIH4",
+  authDomain: "instan-cermelang.firebaseapp.com",
+  projectId: "instan-cermelang",
+  storageBucket: "instan-cermelang.appspot.com",
+  messagingSenderId: "484031496322",
+  appId: "1:484031496322:web:2526caec7a1e7527dfb49b",
+  measurementId: "G-SCREWE22XP"
+};
